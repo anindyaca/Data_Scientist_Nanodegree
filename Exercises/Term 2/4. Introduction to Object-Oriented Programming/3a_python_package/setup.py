d5443b657ee0ae4a2e79885eec12dc7c8e0a1123
anindyachakraborty <anindya.catalyst@gmail.com>
2023-01-04 21:52:02 +0530

    test

--- Exercises/Term 2/4. Introduction to Object-Oriented Programming/3a_python_package/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='distributions',
-      version='0.1',
-      description='Gaussian distributions',
-      packages=['distributions'],
-      # project cannot be safely installed and run from a zip file
-      zip_safe=False)
